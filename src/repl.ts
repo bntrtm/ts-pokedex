8ba39280fa3d943c6fb139d88a11a16f0c954b74
bntrtm <git.bntrtm@tjbainter.com>
2026-03-17 15:49:06 -0500

    feat: add basic REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,24 @@
+import { createInterface } from "node:readline";
+
 export function cleanInput(input: string): string[] {
   return input.toLowerCase().trim().split(" ").filter((word) => word !== "");
+}
+
+export function startREPL(): void {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+  rl.on("line", (line) => {
+    const words = cleanInput(line);
+    if (words.length === 0) {
+      return
+    } else {
+      console.log(`Your command was: ${words[0]}`)
+    }
+    rl.prompt();
+  })
 }
