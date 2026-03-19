21386bf8ddcb566e3ba1764bde235135b973c7f0
bntrtm <git.bntrtm@tjbainter.com>
2026-03-18 22:05:15 -0500

    fix: update map locations after navigation
    map navigation was not updating properly with new values

--- src/command_map.ts
+++ src/command_map.ts
@@ -2,16 +2,25 @@ import { State } from "./state.js"
 
 export async function commandMap(state: State): Promise<void> {
   const locations = await state.api.fetchLocations(state.nextLocationsURL);
+
+  state.nextLocationsURL = locations.next;
+  state.prevLocationsURL = locations.previous;
+
   for (const l of locations.results) {
     console.log(l.name);
   }
 };
 
 export async function commandMapb(state: State): Promise<void> {
-  if (state.prevLocationsURL === "") {
+  if (!state.prevLocationsURL) {
     console.log(`You're on the first page.`)
   }
+
   const locations = await state.api.fetchLocations(state.prevLocationsURL);
+
+  state.nextLocationsURL = locations.next;
+  state.prevLocationsURL = locations.previous;
+
   for (const l of locations.results) {
     console.log(l.name);
   }
