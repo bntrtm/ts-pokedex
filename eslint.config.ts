f73e0917fdb26b412f990f67675e4f9dfebcfbd0
bntrtm <git.bntrtm@tjbainter.com>
2026-03-23 15:45:14 -0500

    chore: set up prettier formatter

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,7 @@
 import love from "eslint-config-love";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-plugin-prettier";
 
 export default [
   {
@@ -15,13 +18,20 @@ export default [
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+    },
     rules: {
+      ...prettierConfig.rules,
       "no-console": "off",
       "object-shorthand": "off",
       "no-magic-numbers": "off",
       "@typescript-eslint/no-magic-numbers": ["error", { ignore: [-1, 0, 1] }],
       "@typescript-eslint/prefer-destructuring": "off",
       "@typescript-eslint/no-unsafe-type-assertion": "off",
+
+      "prettier/prettier": "error",
     },
   },
 ];
