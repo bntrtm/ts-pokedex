b3e7c5e48621ebac5ac403c30de8b49db0b3f440
bntrtm <git.bntrtm@tjbainter.com>
2026-03-27 16:14:03 -0500

    test: update verb formatting test

--- src/formatting_verbs.test.ts
+++ src/formatting_verbs.test.ts
@@ -1,16 +1,18 @@
 import { interpStringVerbs, sVerb } from "./formatting_verbs.js";
 import { test } from "vitest";
 
-// these tests use a hypothetical 'trainers' endpoint,
-// to test for nested resources as one might find in a REST API.
+// these tests use a hypothetical endpoints to test 
+// for nested resources, as one might find in a REST API.
 
 const baseURL = "https://example.com";
+const apiURL = `${baseURL}/api`;
 const pathParam = `/${sVerb}`;
-const testURLTrainers = `${baseURL}/trainers`;
+
+const testURLTrainers = `${apiURL}/trainers`;
 const testURLTrainer = testURLTrainers + pathParam;
 
 // damn you, invariant plurality!
-const testURLTrainerPokemons = `${baseURL}/pokemon`;
+const testURLTrainerPokemons = `${testURLTrainer}/pokemon`;
 const testURLTrainerPokemon = testURLTrainerPokemons + pathParam;
 
 test("verb interp with no operands throws err", ({ expect }) => {
@@ -19,16 +21,12 @@ test("verb interp with no operands throws err", ({ expect }) => {
 
 test("verb interp with too many operands throws err", ({ expect }) => {
   expect(() =>
-    interpStringVerbs(testURLTrainerPokemons, "ash", "pikachu"),
+    interpStringVerbs(testURLTrainerPokemons, "ash_id", "pikachu_id"),
   ).toThrow(/reads arg/v);
 });
 
 test("verb interp returns expected string", ({ expect }) => {
-  const interpolated = interpStringVerbs(
-    testURLTrainerPokemon,
-    "ash",
-    "pikachu",
-  );
-  const expected = "https://example.com/trainers/ash/pokemon/pikachu";
+  const interpolated = interpStringVerbs(testURLTrainerPokemon, "ash_id", "pikachu_id");
+  const expected = "https://example.com/api/trainers/ash_id/pokemon/pikachu_id";
   expect(interpolated).toBe(expected);
 });
